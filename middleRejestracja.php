62ebe57477544cc65466daaaa63805ec42189a02
Pawel <pawel.chorzewski1@gmail.com>
2021-04-21 20:04:27 +0200

    Dodanie formularza rejestracji

---
+++ middleRejestracja.php
@@ -0,0 +1,26 @@
+<div class="container">
+<div class="row my-1">
+    <h2 class="text-center">Rejestracja użytkownika</h2>
+<div>
+<div class="row my-1">
+<form>
+    <div class="mb-3">
+        <label for="login" class="form-label">Nazwa użytkownika</label>
+        <input type="text" class="form-control" id="login" name="login">
+    </div>
+    <div class="mb-3">
+        <label for="nazwisko" class="form-label">Hasło</label>
+        <input type="text" class="form-control" id="nazwisko">
+    </div>
+    <div class="mb-3">
+        <label for="nazwisko" class="form-label">Powtórz hasło</label>
+        <input type="text" class="form-control" id="nazwisko">
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">E-mail</label>
+        <input type="email" class="form-control" id="email" placeholder="name@example.com">
+    </div>
+    <button type="sumbit" class="btn btn-success">Zarejestruj</button>
+</form>
+</div>
+</div>
