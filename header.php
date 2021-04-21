e3e53f35a782fca70337a95a49e283d5a147c93e
Pawel <pawel.chorzewski1@gmail.com>
2021-04-21 13:30:59 +0200

    Utworzenie nawigacji dla użytkownika niezalogowanego

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <title>Quiz</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+  <div class="container-fluid">
+    <a class="navbar-brand" ><b>QuizApp</b></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#appNav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="appNav">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Strona główna</a>
+        </li>
+        
+      </ul>
+      
+        <a class="btn btn-outline-light" type="submit">Zaloguj</a>
+        <a class="btn btn-outline-light" type="submit">Zarejestruj</a>
+    </div>
+  </div>
+</nav>
