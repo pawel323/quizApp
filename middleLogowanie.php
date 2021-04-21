83c5f2a41baf448ef4e0347f9843a9c13ea51c2d
Pawel <pawel.chorzewski1@gmail.com>
2021-04-21 20:34:28 +0200

    Utworzenie formularza logowania

---
+++ middleLogowanie.php
@@ -0,0 +1,18 @@
+<div class="container">
+<div class="row my-1">
+    <h2 class="text-center">Logowanie</h2>
+<div>
+<div class="row my-1">
+<form>
+    <div class="mb-3">
+        <label for="login" class="form-label">Nazwa użytkownika</label>
+        <input type="text" class="form-control" id="login" name="login">
+    </div>
+    <div class="mb-3">
+        <label for="nazwisko" class="form-label">Hasło</label>
+        <input type="text" class="form-control" id="nazwisko">
+    </div>
+    <button type="sumbit" class="btn btn-success">Zaloguj</button>
+</form>
+</div>
+</div>
