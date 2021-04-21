eafd8ec5e9c332e14be132cdeb0fb528d00a5afb
Pawel <pawel.chorzewski1@gmail.com>
2021-04-21 20:34:10 +0200

    Dodanie strony logowania

---
+++ logowanie.php
@@ -0,0 +1,5 @@
+<?php
+if(file_exists('header.php')) include ('header.php');
+if(file_exists('middleLogowanie.php')) include ('middleLogowanie.php');
+if(file_exists('footer.php')) include ('footer.php');
+?>
