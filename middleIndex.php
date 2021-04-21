b0cad0b91ac844a60b35632b4a72d116a5de40e3
Pawel <pawel.chorzewski1@gmail.com>
2021-04-21 19:46:36 +0200

    Stworzenie strony głównej serwisu

---
+++ middleIndex.php
@@ -0,0 +1,68 @@
+<div class="container-fluid">
+<div class="row my-1">
+    <h2 class="text-center">Witamy w QuizApp</h2>
+<div>
+<div class="row my-1">
+    <div class="col-sm-6 offset-sm-3">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <h5 class="text-center">Sprawdź swoją wiedzę</h5>
+            </div>
+            <div class="carousel-item">
+                <h5 class="text-center">Różne dziedziny</h5>
+            </div>
+            <div class="carousel-item">
+                <h5 class="text-center">Testy jednokrotnego wyboru</h5>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
+<div class="row bg-success">
+    <div class="col-sm-4">
+        <img src="img/rozwiazywanie.jpg" style="width: 100%;height: auto;" class="img-thumbnail img-fluid my-1">
+        <h5 style="color:white" class="text-center">Rozwiązuj quizy</h5>
+    </div>
+    <div class="col-sm-4">
+    <img src="img/wyszukiwanie.jpg" style="width: 100%;height:auto;" class="img-thumbnail img-fluid my-1">
+        <h5 style="color:white" class="text-center">Wyszukaj konkretny quiz według identyfikatora</h5>
+    </div>
+    <div class="col-sm-4">
+    <img src="img/podium.png" style="width: 100%;height:auto" class="img-thumbnail img-fluid my-1">
+        <h5 style="color:white" class="text-center">Porównuj swoje wyniki z innymi</h5>
+    </div>
+    <div class="col-sm-10 offset-sm-1">
+        <p style="color:white" class="text-center">*Tylko dla zalogowanych użytkowników</p>
+    </div>
+</div>
+
+<div class="row my-1">
+<h4 class="my-5">Chcesz dołączyć do grona twórców?</h4>
+    <div class="col-sm-2">
+        <p>Wypełnij formularz, opowiedz nam coś o sobie. Skontaktujemy się z Tobą za pośrednictwem e-maila</p>
+    </div>
+    <div class="col-sm-10">
+    <form>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Imię</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Nazwisko</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">E-mail</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">O mnie</label>
+        <textarea class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" autocorrect=on rows=5></textarea>
+    </div>
+    </form>
+    </div>
+</div>
+
+
+</div>
