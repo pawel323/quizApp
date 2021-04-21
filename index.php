04a2430b70a9f6085b217bfb13dcd3330cbe3c65
Pawel <pawel.chorzewski1@gmail.com>
2021-04-21 13:19:41 +0200

    Zmiana koncepcji

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-session_start();
-if(isset($_SESSION['zalogowany'])){
-    if(file_exists('header.php')) include ('header.php');
-    if(file_exists('middleIndex.php')) include ('middleIndex.php');
-    if(file_exists('footer.php')) include ('footer.php');
-}
-else
-header('Location: rejestracja.php');
+if(file_exists('header.php')) include ('header.php');
+if(file_exists('middleIndex.php')) include ('middleIndex.php');
+if(file_exists('footer.php')) include ('footer.php');
 ?>
