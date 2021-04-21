7a19b703ca0b66f8a9252a98248b3bf0d5b3c346
Pawel <pawel.chorzewski1@gmail.com>
2021-04-21 20:04:49 +0200

    Utworzenie strony rejestracji

---
+++ rejestracja.php
@@ -0,0 +1,5 @@
+<?php
+if(file_exists('header.php')) include ('header.php');
+if(file_exists('middleRejestracja.php')) include ('middleRejestracja.php');
+if(file_exists('footer.php')) include ('footer.php');
+?>
