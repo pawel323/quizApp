2fd719929e7269148d81fca4c8bd55ff9201d521
Pawel <pawel.chorzewski1@gmail.com>
2021-04-22 12:01:11 +0200

    Dodanie wymagalności pól w formularzu rejestracji

--- middleRejestracja.php
+++ middleRejestracja.php
@@ -6,19 +6,19 @@
 <form>
     <div class="mb-3">
         <label for="login" class="form-label">Nazwa użytkownika</label>
-        <input type="text" class="form-control" id="login" name="login">
+        <input type="text" class="form-control" id="login" name="login" required>
     </div>
     <div class="mb-3">
         <label for="nazwisko" class="form-label">Hasło</label>
-        <input type="text" class="form-control" id="nazwisko">
+        <input type="text" class="form-control" id="nazwisko" required>
     </div>
     <div class="mb-3">
         <label for="nazwisko" class="form-label">Powtórz hasło</label>
-        <input type="text" class="form-control" id="nazwisko">
+        <input type="text" class="form-control" id="nazwisko" required>
     </div>
     <div class="mb-3">
         <label for="email" class="form-label">E-mail</label>
-        <input type="email" class="form-control" id="email" placeholder="name@example.com">
+        <input type="email" class="form-control" id="email" placeholder="name@example.com" required>
     </div>
     <button type="sumbit" class="btn btn-success">Zarejestruj</button>
 </form>
