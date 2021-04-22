8a9ce3ba838a3d033c78d1b50b6a68063218f005
Pawel <pawel.chorzewski1@gmail.com>
2021-04-22 13:35:13 +0200

    Dodanie skryptu rejestrującego użytkownika

--- rejestracja.php
+++ rejestracja.php
@@ -1,4 +1,5 @@
 <?php
+if(file_exists('rejestracjaSkrypt.php')) include ('rejestracjaSkrypt.php');
 if(file_exists('header.php')) include ('header.php');
 if(file_exists('middleRejestracja.php')) include ('middleRejestracja.php');
 if(file_exists('footer.php')) include ('footer.php');
