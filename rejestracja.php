0a97c8f1e335bda20a1cd86eaa3761342f90b3e5
Pawel <pawel.chorzewski1@gmail.com>
2021-04-22 18:46:50 +0200

    Usunięcie odniesienia do nieistniejącego pliku

--- rejestracja.php
+++ rejestracja.php
@@ -112,7 +112,6 @@ if (isset($_POST['login']) && ($_POST['pass'] == $_POST['passRepeat']))
     }
     
 }
-if(file_exists('rejestracjaSkrypt.php')) include ('rejestracjaSkrypt.php');
 if(file_exists('header.php')) include ('header.php');
 if(file_exists('middleRejestracja.php')) include ('middleRejestracja.php');
 if(file_exists('footer.php')) include ('footer.php');
