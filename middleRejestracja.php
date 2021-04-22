70d1c20adbc2416dfffaf79f3035e753f4a45e5a
Pawel <pawel.chorzewski1@gmail.com>
2021-04-22 13:34:36 +0200

    Wyświetlanie komunikatów o walidacji danych

--- middleRejestracja.php
+++ middleRejestracja.php
@@ -1,24 +1,52 @@
 <div class="container">
 <div class="row my-1">
+<?php
+			if (isset($_SESSION['udanarejestracja']))
+			{
+				echo '<div>'.$_SESSION['udanarejestracja'].'</div>';
+				unset($_SESSION['udanarejestracja']);
+			}
+?>
     <h2 class="text-center">Rejestracja użytkownika</h2>
 <div>
 <div class="row my-1">
-<form>
+<form method="POST">
     <div class="mb-3">
         <label for="login" class="form-label">Nazwa użytkownika</label>
         <input type="text" class="form-control" id="login" name="login" required>
+        <?php
+			if (isset($_SESSION['e_nick']))
+			{
+				echo '<div>'.$_SESSION['e_nick'].'</div>';
+				unset($_SESSION['e_nick']);
+			}
+		?>
     </div>
     <div class="mb-3">
         <label for="nazwisko" class="form-label">Hasło</label>
-        <input type="text" class="form-control" id="nazwisko" required>
+        <input type="text" class="form-control" id="nazwisko" name="pass" required>
+        <?php
+			if (isset($_SESSION['e_haslo']))
+			{
+				echo '<div>'.$_SESSION['e_haslo'].'</div>';
+				unset($_SESSION['e_haslo']);
+			}
+		?>	
     </div>
     <div class="mb-3">
         <label for="nazwisko" class="form-label">Powtórz hasło</label>
-        <input type="text" class="form-control" id="nazwisko" required>
+        <input type="text" class="form-control" id="nazwisko" name="passRepeat" required>
     </div>
     <div class="mb-3">
         <label for="email" class="form-label">E-mail</label>
-        <input type="email" class="form-control" id="email" placeholder="name@example.com" required>
+        <input type="email" class="form-control" id="email" placeholder="name@example.com" name="email" required>
+        <?php
+			if (isset($_SESSION['e_email']))
+			{
+				echo '<div>'.$_SESSION['e_email'].'</div>';
+				unset($_SESSION['e_email']);
+			}
+		?>
     </div>
     <button type="sumbit" class="btn btn-success">Zarejestruj</button>
 </form>
